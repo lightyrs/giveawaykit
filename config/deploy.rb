bab3aa4f7a0c43e43c6d1cb47f003b3462257b8c
Harris Novick <harris@giveawaykit.com>
2014-07-20 21:03:52 -0400

    basic working capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,13 @@
-# config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
+set :application, 'giveawaykit'
+set :repo_url, 'git@github.com:giveawaykit/giveawaykit.git'
+set :branch, 'master'
+set :deploy_to, '/home/gk/giveawaykit'
+set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+set :keep_releases, 5
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
@@ -31,8 +18,6 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
