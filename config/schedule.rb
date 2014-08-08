edf3b87c3458c24090c0e789ead4f34c97536a03
Harris Novick <harris@giveawaykit.com>
2014-08-07 22:56:58 -0400

    fix syntax errors in bash commands

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ env :PATH, ENV['PATH']
 every 1.weeks do
   case @environment
     when 'production'
-      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayOrphansWorker.perform_async'"
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit; fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayOrphansWorker.perform_async'"
     else
       runner 'GiveawayOrphansWorker.perform_async'
   end
@@ -12,7 +12,7 @@ end
 every 1.hours do
   case @environment
     when 'production'
-      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'SubscriptionScheduleWorker.perform_async'"
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit; fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'SubscriptionScheduleWorker.perform_async'"
     else
       runner 'SubscriptionScheduleWorker.perform_async'
   end
@@ -21,7 +21,7 @@ end
 every 30.minutes do
   case @environment
     when 'production'
-      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'RefreshWorker.perform_async'"
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit; fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'RefreshWorker.perform_async'"
     else
       runner 'RefreshWorker.perform_async'
   end
@@ -30,7 +30,7 @@ end
 every 5.minutes do
   case @environment
     when 'production'
-      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"unpublish\")'; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"publish\")';"
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit; fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"unpublish\")'; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"publish\")';"
     else
       runner 'GiveawayScheduleWorker.perform_async("unpublish")'
       runner 'GiveawayScheduleWorker.perform_async("publish")'
