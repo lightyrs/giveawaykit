b3f0d308dfa67de5a79a2ccc54a0b27eb0749929
Harris Novick <harris@giveawaykit.com>
2014-08-07 22:34:52 -0400

    update whenever gem to use explicit commands in production

--- app/workers/giveaway_schedule_worker.rb
+++ app/workers/giveaway_schedule_worker.rb
@@ -3,7 +3,6 @@ class GiveawayScheduleWorker
   sidekiq_options queue: :often
 
   def perform(method)
-    puts ENV['PATH'].inspect rescue nil
     Giveaway.schedule_worker(method)
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,38 @@
 env :PATH, ENV['PATH']
 
 every 1.weeks do
-  runner 'GiveawayOrphansWorker.perform_async'
+  case @environment
+    when 'production'
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayOrphansWorker.perform_async'"
+    else
+      runner 'GiveawayOrphansWorker.perform_async'
+  end
 end
 
 every 1.hours do
-  runner 'SubscriptionScheduleWorker.perform_async'
+  case @environment
+    when 'production'
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'SubscriptionScheduleWorker.perform_async'"
+    else
+      runner 'SubscriptionScheduleWorker.perform_async'
+  end
 end
 
 every 30.minutes do
-  runner 'RefreshWorker.perform_async'
+  case @environment
+    when 'production'
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'RefreshWorker.perform_async'"
+    else
+      runner 'RefreshWorker.perform_async'
+  end
 end
 
 every 5.minutes do
-  runner 'GiveawayScheduleWorker.perform_async("unpublish")'
-  runner 'GiveawayScheduleWorker.perform_async("publish")'
+  case @environment
+    when 'production'
+      command "source /usr/local/rvm/scripts/rvm; if [ -f /etc/default/giveawaykit ]; then . /etc/default/giveawaykit fi; cd /home/gk/giveawaykit/current; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"unpublish\")'; bundle exec rails runner 'GiveawayScheduleWorker.perform_async(\"publish\")';"
+    else
+      runner 'GiveawayScheduleWorker.perform_async("unpublish")'
+      runner 'GiveawayScheduleWorker.perform_async("publish")'
+  end
 end
