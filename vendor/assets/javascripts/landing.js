02e2b446992c2a7ab46e95c4be46049cb7e76758
Harris Novick <harris@giveawaykit.com>
2014-08-16 18:23:34 -0400

    marketing copy and mailchimp form

--- vendor/assets/javascripts/landing.js
+++ vendor/assets/javascripts/landing.js
@@ -1,12 +1,11 @@
-jQuery.extend( jQuery.easing,
-{
-    def: 'easeOutQuad',
-    easeInOutExpo: function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    }
+jQuery.extend( jQuery.easing, {
+  def: 'easeOutQuad',
+  easeInOutExpo: function (x, t, b, c, d) {
+    if (t==0) return b;
+    if (t==d) return b+c;
+    if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+    return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+  }
 });
 
 !function ($) {
@@ -14,15 +13,14 @@ jQuery.extend( jQuery.easing,
   $(function(){
 
     $(document).on('click.app','[href^="#"]',function (e) {
-        e.preventDefault();
-        var $target = this.hash;
-        if(!$(this).data('jump')) return;
-        $('html, body').stop().animate({
-            'scrollTop': $($target).offset().top
-        }, 1000, 'easeInOutExpo', function () {
-            window.location.hash = $target;
-        });
+      e.preventDefault();
+      var $target = this.hash;
+      if(!$(this).data('jump')) return;
+      $('html, body').stop().animate({
+        'scrollTop': $($target).offset().top
+      }, 1000, 'easeInOutExpo', function () {
+        window.location.hash = $target;
+      });
     });
-
   });
 }(window.jQuery);
